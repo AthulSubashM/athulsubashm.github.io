73bbe8120ccc81cfc0f6b9a287c1b5423bf3da54
AthulSubashM <athul.m.subash@gmail.com>
2025-10-07 17:02:44 -0400

    slider and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Home/Hero/Hero";
 import Techstack from "../components/Home/Techstack";
 import About from "../components/Home/About";
+import Projects from "../components/Home/ProjectsView";
 
 function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
       <Techstack />
     </>
   );
